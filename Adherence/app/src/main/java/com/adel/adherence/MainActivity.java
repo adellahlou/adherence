14c95961f84661a6a906acd4df7083ae79390641
Adel Lahlou <adellahlou@users.noreply.github.com>
2015-01-22 00:07:04 -0600

    Skeleton Serial, Batch, and OTA From Laird
    Gutted the Laird Toolkit App and skeletoned it to use it’s serial app.

--- Adherence/app/src/main/java/com/adel/adherence/MainActivity.java
+++ Adherence/app/src/main/java/com/adel/adherence/MainActivity.java
@@ -1,17 +1,10 @@
 package com.adel.adherence;
 
-import android.app.AlertDialog;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
-import android.bluetooth.BluetoothSocket;
-import android.content.DialogInterface;
-import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.provider.Settings;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -20,9 +13,6 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.io.IOException;
-import java.net.Socket;
-import java.util.Set;
 import java.util.UUID;
 
 
@@ -52,10 +42,56 @@ public class MainActivity extends ActionBarActivity {
                     .commit();
         }
 
-        setupBluetooth();
+        //setupBluetooth();
     }
 
-    private void setupBluetooth(){
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.bt_test, container, false);
+
+            return rootView;
+        }
+    }
+
+}
+
+
+
+
+/* private void setupBluetooth(){
         btadapter = BluetoothAdapter.getDefaultAdapter();
 
         if (!(btadapter == null)) {
@@ -126,7 +162,7 @@ public class MainActivity extends ActionBarActivity {
             } catch (IOException e) { }
         }
     }
-/*
+
     private class ConnectBT extends AsyncTask<BluetoothDevice, Void, String> {
 
         private BluetoothSocket btSocket;
@@ -165,45 +201,3 @@ public class MainActivity extends ActionBarActivity {
 
     }
 //*/
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.bt_test, container, false);
-
-            return rootView;
-        }
-    }
-
-}
